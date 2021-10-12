51dd2c1fd40db28485487fe44b831af01405d62b
Pushpasis Sarkar <spushpasis@vmware.com>
2021-10-12 03:33:50 -0700

    mgmtd: Bringup MGMTD daemon and db module support
    Features added in this commit:
    1. Bringup/shutdown new management daemon 'mgmtd' along with FRR.
    2. Support for Startup, Candidate and Running DBs.
    3. Lock/Unlock DB feature using pthread lock.
    4. Load config from a JSON file onto candidate DB.
    5. Save config to a JSON file from running/candidate DB.
    6. Dump candidate or running DB contents on the terminal or a file in
       JSON/XML format.
    7. Maintaining commit history (Full rollback support to be added in
       future commits).
    8. Addition of debug commands.
    
    Co-authored-by: Pushpasis Sarkar <spushpasis@vmware.com>
    Co-authored-by: Abhinay Ramesh <rabhinay@vmware.com>
    Co-authored-by: Ujwal P <ujwalp@vmware.com>
    Signed-off-by: Yash Ranjan <ranjany@vmware.com>

---
+++ mlag/mlag_pb.c
@@ -0,0 +1,26 @@
+/*
+ * Dummy main source file mlag/libmlag_pb.la.
+ * Copyright (C) 2021  Vmware, Inc.
+ *		       Pushpasis Sarkar
+ *
+ * This program is free software; you can redistribute it and/or modify it
+ * under the terms of the GNU General Public License as published by the Free
+ * Software Foundation; either version 2 of the License, or (at your option)
+ * any later version.
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+ * FITNESS FOR A PARTICULAR PURPOSE.  See the GNU General Public License for
+ * more details.
+ *
+ * You should have received a copy of the GNU General Public License along
+ * with this program; see the file COPYING; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA 02110-1301 USA
+ */
+
+/*
+ * NOTE: This file serves as blank dummy source file to be included
+ * in mlag/libmlag_pb.la by default. In platforms where PROTOBUF Version 3
+ * has been enabled, the actual protobuf generated source mlag/mlag.pb-c.c
+ * shall be included in it too. Please see mlag/subdir.am for more details.
+ */
